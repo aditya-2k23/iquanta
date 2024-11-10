d02bd0f21412d7c0211af4c5aee27da8be93df80
aditya-2k23 <holaaditya123@gmail.com>
2024-11-11 00:31:49 +0530

    Add Hero component to Home page for enhanced visual appeal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="container h-screen mx-auto bg-[#ededed]">
       <Header />
+      <Hero />
     </div>
   );
 }
