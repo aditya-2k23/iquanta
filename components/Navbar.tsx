286834043f4552c53ec0bd2edc828be9cc01a3e4
aditya-2k23 <holaaditya123@gmail.com>
2024-11-10 15:23:41 +0530

    Refactor Navbar component to accept children and adjust styling for improved layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,15 +18,11 @@ const NavItem = ({
           xmlns="http://www.w3.org/2000/svg"
           width="12"
           height="12"
-          fill="#878787"
-          className="bi bi-chevron-down"
           viewBox="0 0 16 16"
-          strokeWidth="2"
         >
           <path
             fillRule="evenodd"
             d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-            fill="#878787"
           />
         </svg>
       </a>
@@ -43,24 +39,24 @@ const NavItem = ({
   );
 };
 
-const Navbar = () => {
+const Navbar = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center p-3">
+    <div className="flex items-center pl-32">
       {/* Logo Section */}
       <div className="logo-container">
         <a href="https://www.iquanta.in/blog/">
           <Image
             src="/black_logo-1.webp"
             alt="Logo"
-            width={170}
-            height={172}
+            width={150}
+            height={170}
             className="mr-10"
           />
         </a>
       </div>
 
       {/* Navigation Menu */}
-      <nav className="menu-container">
+      <nav className="flex gap-10 items-center">
         <ul className="list-none flex gap-10">
           <NavItem
             title="CAT & Non-CAT"
@@ -97,6 +93,8 @@ const Navbar = () => {
             ]}
           />
         </ul>
+
+        {children}
       </nav>
     </div>
   );
