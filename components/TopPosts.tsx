4ecdc9ca4ce9a38744dc708b589478da9c709a60
aditya-2k23 <holaaditya123@gmail.com>
2024-11-13 13:09:09 +0530

    Refactor TopPosts component for improved layout and styling

--- components/TopPosts.tsx
+++ components/TopPosts.tsx
@@ -9,27 +9,27 @@ interface PostProps {
 
 const TopPosts: React.FC<PostProps> = ({ title, link, imgUrl, category }) => {
   return (
-    <div className="w-full h-28 mb-5 p-2">
-      <div className="flex items-center justify-center gap-2">
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            <div
-              className="h-28 w-28 bg-cover bg-center"
-              style={{ backgroundImage: `url(${imgUrl})` }}
-            ></div>
+    <div className="w-full h-24 mb-5 p-2">
+      <div className="flex justify-center gap-2">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <div
+            className="h-24 w-24 bg-cover bg-center"
+            style={{ backgroundImage: `url(${imgUrl})` }}
+          ></div>
+        </a>
+        <div className="ml-1">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xl font-bold leading-6 hover:text-red-600"
+          >
+            {title}
           </a>
-          <div className="ml-1">
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-xl font-bold hover:text-red-600"
-            >
-              {title}
-            </a>
-            <div className="mt-2 text-sm font-semibold text-red-600">
-              <span>{category}</span>
-            </div>
+          <div className="mt-2 text-sm font-semibold text-red-600">
+            <span>{category}</span>
           </div>
+        </div>
       </div>
     </div>
   );
