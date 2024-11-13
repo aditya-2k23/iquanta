0e2aaf7a4a8bc39800a9116566dbad223bb72b9d
aditya-2k23 <holaaditya123@gmail.com>
2024-11-13 13:49:38 +0530

    Add remote image pattern for iquanta blog uploads in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.iquanta.in",
+        port: "",
+        pathname: "/blog/wp-content/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
