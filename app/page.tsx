3b5d2fc98896f64d651f569451cdc5e669b51d3a
aditya-2k23 <holaaditya123@gmail.com>
2024-11-15 01:35:57 +0530

    Add Footer and LatestPosts components to Home page for enhanced content display

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import DontMiss from "@/components/DontMiss";
 import Exams from "@/components/Exams";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import LatestPosts from "@/components/LatestPosts";
 
 export default function Home() {
   return (
@@ -10,6 +12,11 @@ export default function Home() {
       <Hero />
       <DontMiss />
       <Exams />
+      <h1 className="uppercase italic text-red-500 font-bold text-5xl text-center my-10 p-2">
+        Latest Posts
+      </h1>
+      <LatestPosts />
+      <Footer />
     </div>
   );
 }
