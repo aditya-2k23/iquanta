c5816d27aaa6c43660015ee3d0c7e52054afc383
aditya-2k23 <holaaditya123@gmail.com>
2024-11-15 14:51:57 +0530

    Enhance Footer component styling and structure for improved readability and user experience

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,26 +12,29 @@ const Article = ({
   date: string;
   category: string;
 }) => (
-  <div className="flex flex-col space-x-4">
-    <h4 className="text-xl font-semibold">
-      <a className="hover:text-blue-400" href={href}>
+  <div className="flex flex-col">
+    <h4 className="text-xl mb-2 font-semibold leading-6">
+      <a className="hover:text-blue-500" href={href}>
         {title}
       </a>
     </h4>
-    <p className="text-sm text-gray-400">
-      {date} - {category}
+    <p className="text-xs font-semibold">
+      <a className="mr-1 uppercase cursor-pointer hover:text-red-500 text-gray-800">
+        {category}
+      </a>{" "}
+      <span className="text-gray-600">{date}</span>
     </p>
   </div>
 );
 
 const Footer = () => {
   return (
-    <footer className="border-t mt-10 border-black p-6">
+    <footer className="border-t mt-10 border-black pt-10 pb-5 px-16">
       <div className="w-full">
-        <div className="space-y-6 lg:flex lg:justify-between lg:gap-12 flex-shrink-0 lg:space-y-0">
+        <div className="space-y-6 pb-2 lg:flex lg:justify-between lg:gap-12 flex-shrink-0 lg:space-y-1">
           {/* About Us Section */}
           <div className="flex-1 space-y-4">
-            <h3 className="text-xl uppercase font-semibold">About us</h3>
+            <h3 className="text-2xl uppercase font-semibold">About us</h3>
             <p className="font-serif">
               Get your questions answered about learning with iQuanta. Call: +91
               76830 43155
@@ -69,7 +72,9 @@ const Footer = () => {
 
           {/* Latest Articles */}
           <div className="flex-1 space-y-4">
-            <h3 className="text-xl font-semibold">Latest Articles</h3>
+            <h3 className="text-2xl uppercase font-semibold">
+              Latest Articles
+            </h3>
             <div className="space-y-4">
               <Article
                 href="https://www.iquanta.in/blog/cat-99-96iler-iim-ahmedabad-convert-kislay/"
@@ -79,22 +84,22 @@ const Footer = () => {
               />
               <Article
                 href="https://www.iquanta.in/blog/clat-mini-mock-series-by-iquanta-13th-november-2024/"
-                title="CLAT Mini Mock Series by iQuanta"
+                title="CLAT Mini Mock Series by iQuanta: 14th November 2024"
                 date="November 14, 2024"
-                category="CLAT Mini Mock"
+                category="CLAT Mini Mock Series"
               />
               <Article
                 href="https://www.iquanta.in/blog/clat-mini-mock-series-by-iquanta-13th-november-2024/"
                 title="CLAT Mini Mock Series by iQuanta: 13th November 2024"
                 date="November 14, 2024"
-                category="CLAT Mini Mock"
+                category="CLAT Mini Mock Series"
               />
             </div>
           </div>
 
           {/* Most Popular Section */}
           <div className="flex-1 space-y-4">
-            <h3 className="text-xl font-semibold">Most Popular</h3>
+            <h3 className="text-2xl uppercase font-semibold">Most Popular</h3>
             <div className="space-y-4">
               <Article
                 href="https://www.iquanta.in/blog/cat-99-96iler-iim-ahmedabad-convert-kislay/"
@@ -104,29 +109,29 @@ const Footer = () => {
               />
               <Article
                 href="https://www.iquanta.in/blog/clat-mini-mock-series-by-iquanta-13th-november-2024/"
-                title="CLAT Mini Mock Series by iQuanta"
+                title="CLAT Mini Mock Series by iQuanta: 14th November 2024"
                 date="November 14, 2024"
-                category="CLAT Mini Mock"
+                category="CLAT Mini Mock Series"
               />
               <Article
                 href="https://www.iquanta.in/blog/clat-mini-mock-series-by-iquanta-13th-november-2024/"
                 title="CLAT Mini Mock Series by iQuanta: 13th November 2024"
                 date="November 14, 2024"
-                category="CLAT Mini Mock"
+                category="CLAT Mini Mock Series"
               />
             </div>
           </div>
 
           <div className="flex-1 space-y-4">
-            <h3>Subscribe</h3>
+            <h3 className="uppercase font-bold text-2xl">Subscribe</h3>
 
             <div className="mt-4 flex flex-col">
               <input
                 type="text"
                 placeholder="Email address"
-                className="w-full mb-2 p-1"
+                className="w-full mb-2 px-2 py-3 border border-black placeholder:font-serif placeholder:text-sm placeholder:text-black"
               />
-              <button className="uppercase font-semibold text-sm w-full text-white py-2 px-5 bg-red-500">
+              <button className="uppercase font-semibold w-full text-white py-3 bg-red-500 hover:bg-red-700">
                 I want in {"->"}
               </button>
             </div>
@@ -134,9 +139,9 @@ const Footer = () => {
               <input
                 type="checkbox"
                 id="check"
-                className="form-checkbox h-4 w-4 text-red-500"
+                className="form-checkbox cursor-pointer h-3 w-3 text-red-500"
               />
-              <span className="text-sm">
+              <span className="text-sm cursor-pointer font-serif">
                 I&apos;ve read and accept the{" "}
                 <a className="hover:underline text-red-500 hover:text-red-700">
                   Privacy Policy
