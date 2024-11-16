506518a97fd278e970c8ed3cfdb82415419ea1d2
aditya-2k23 <holaaditya123@gmail.com>
2024-11-16 14:21:58 +0530

    Refactor Header component for improved search overlay layout and styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
-    <header className="relative bg-white w-full p-7">
+    <header className="relative bg-white w-full p-4 md:p-7">
       {/* Navigation Menu */}
       <Navbar>
         <div className="flex justify-end">
@@ -21,28 +21,25 @@ const Header = () => {
 
         {/* Search Overlay */}
         {isSearchOpen && (
-          <>
-            <div className=""></div>
-            <div className="absolute z-20 bg-white border-t-2 border-[#e04040] p-3 shadow-xl">
-              <form
-                method="get"
-                action="https://www.iquanta.in/blog/"
-                className="flex items-center"
+          <div className="absolute z-20 bg-white border-t-2 border-[#e04040] md:top-[3.5rem] md:left-[33rem] p-3 shadow-xl w-full md:w-auto">
+            <form
+              method="get"
+              action="https://www.iquanta.in/blog/"
+              className="flex flex-col md:flex-row items-center"
+            >
+              <input
+                type="text"
+                autoComplete="off"
+                className="border border-gray-300 py-2 px-3 w-full md:w-auto focus:outline-none"
+              />
+              <button
+                type="submit"
+                className="bg-black text-white px-4 py-2 text-sm mt-2 md:mt-0 md:ml-2 hover:bg-[#e04040] focus:outline-none"
               >
-                <input
-                  type="text"
-                  autoComplete="off"
-                  className="border border-gray-300 py-2 px-3 h-full focus:outline-none"
-                />
-                <button
-                  type="submit"
-                  className="bg-black text-white px-4 py-2 text-sm hover:bg-[#e04040] focus:outline-none"
-                >
-                  Search
-                </button>
-              </form>
-            </div>
-          </>
+                Search
+              </button>
+            </form>
+          </div>
         )}
       </Navbar>
     </header>
